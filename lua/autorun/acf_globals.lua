26ba822886d34c79e56895dfae10472e4931a0d0
Marc-André Fournier Boulet <bouletmarc@hotmail.com>
2013-04-13 17:37:46 -0400

    Quick Fix

--- lua/autorun/acf_globals.lua
+++ lua/autorun/acf_globals.lua
@@ -5,7 +5,7 @@ ACF.AmmoBlacklist = {}
 ACF.Version = 287 -- Make sure to change this as the version goes up or the update check is for nothing! -wrex
 ACF.CurrentVersion = 0 -- just defining a variable, do not change
 --##############
-ACF.Version2 = 15 
+ACF.Version2 = 16 
 ACF.CurrentVersion2 = 0
 print("[[ ACF Loaded ]]")
 
