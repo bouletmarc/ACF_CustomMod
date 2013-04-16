ecf36527220f2b6d220197125ea09a4644334a37
Marc-André Fournier Boulet <bouletmarc@hotmail.com>
2013-04-16 11:36:56 -0400

    Updated to Revision 301

--- lua/autorun/acf_globals.lua
+++ lua/autorun/acf_globals.lua
@@ -2,10 +2,10 @@ ACF = {}
 ACF.AmmoTypes = {}
 ACF.MenuFunc = {}
 ACF.AmmoBlacklist = {}
-ACF.Version = 295 -- Make sure to change this as the version goes up or the update check is for nothing! -wrex
+ACF.Version = 301 -- Make sure to change this as the version goes up or the update check is for nothing! -wrex
 ACF.CurrentVersion = 0 -- just defining a variable, do not change
 --##############
-ACF.Version2 = 19 
+ACF.Version2 = 20 
 ACF.CurrentVersion2 = 0
 print("[[ ACF Loaded ]]")
 
