dacfd108adfc1ce7b7c089d7395f0d5e2d0bcb8c
bouletmarc <bouletmarc@hotmail.com>
2015-05-11 02:08:05 -0400

    ACF Mod 1.2.9 - Possible Better Changelog Show up
    Possible Better Changelog Show up #2

--- lua/autorun/acf_globals_custom.lua
+++ lua/autorun/acf_globals_custom.lua
@@ -2,7 +2,7 @@
 --	Set vars
 --------------------------------------
 ACFCUSTOM = {}
-ACFCUSTOM.Version = 128
+ACFCUSTOM.Version = 129
 ACFCUSTOM.CurrentVersion = 0
 ACFCUSTOM.EngineMakerVersion = 6.1
 ACFC = {}
