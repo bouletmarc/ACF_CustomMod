4d434e125f835fd740bfce9e020616afef70ef26
bouletmarc <bouletmarc@hotmail.com>
2015-05-11 01:28:19 -0400

    ACF Mod 1.2.1 - Added Clutch models to Airplane GB
    Added Clutch models to Airplane Gearbox

--- lua/acf/shared/customs/air-gearbox.lua
+++ lua/acf/shared/customs/air-gearbox.lua
@@ -1,4 +1,5 @@
 
+local MaxTqTiny = 10000
 local MaxTqSmall = 25000
 local MaxTqMedium =	50000
 local MaxTqLarge =	100000
@@ -6,7 +7,7 @@ local MaxTqLarge =	100000
 --AIRPLANE STRAIGHT
 
 ACF_DefineGearboxAir( "air_gearbox-s", {
-	name = "Airplane Gearbox Small",
+	name = "Airplane Gearbox Long Small",
 	desc = "A small and light straight airplane gearbox. Used to Power a propeller.\nYou SHOULD use a gearbox to power this gearbox",
 	model = "models/engines/t5small.mdl",
 	category = "Airplane Gearboxes",
@@ -15,7 +16,7 @@ ACF_DefineGearboxAir( "air_gearbox-s", {
 } )
 
 ACF_DefineGearboxAir( "air_gearbox-m", {
-	name = "Airplane Gearbox Medium",
+	name = "Airplane Gearbox Long Medium",
 	desc = "A medium straight airplane gearbox. Used to Power a propeller.\nYou SHOULD use a gearbox to power this gearbox",
 	model = "models/engines/t5med.mdl",
 	category = "Airplane Gearboxes",
@@ -24,9 +25,47 @@ ACF_DefineGearboxAir( "air_gearbox-m", {
 } )
 
 ACF_DefineGearboxAir( "air_gearbox-l", {
-	name = "Airplane Gearbox Large",
+	name = "Airplane Gearbox Long Large",
 	desc = "A large straight airplane gearbox. Used to Power a propeller.\nYou SHOULD use a gearbox to power this gearbox",
 	model = "models/engines/t5large.mdl",
+	category = "Airplane Gearboxes",
+	weight = 200,
+	maxtq = MaxTqLarge
+} )
+
+--AIRPLANE CLUTCH MODELS
+
+ACF_DefineGearboxAir( "air_gearbox2-t", {
+	name = "Airplane Gearbox Short Tiny",
+	desc = "A  short, tiny and light airplane gearbox. Used to Power a propeller.\nYou SHOULD use a gearbox to power this gearbox",
+	model = "models/engines/flywheelclutcht.mdl",
+	category = "Airplane Gearboxes",
+	weight = 70,
+	maxtq = MaxTqTiny
+} )
+
+ACF_DefineGearboxAir( "air_gearbox2-s", {
+	name = "Airplane Gearbox Short Small",
+	desc = "A  short, small and light airplane gearbox. Used to Power a propeller.\nYou SHOULD use a gearbox to power this gearbox",
+	model = "models/engines/flywheelclutchs.mdl",
+	category = "Airplane Gearboxes",
+	weight = 100,
+	maxtq = MaxTqSmall
+} )
+
+ACF_DefineGearboxAir( "air_gearbox2-m", {
+	name = "Airplane Gearbox Short Medium",
+	desc = "A  short medium airplane gearbox. Used to Power a propeller.\nYou SHOULD use a gearbox to power this gearbox",
+	model = "models/engines/flywheelclutchm.mdl",
+	category = "Airplane Gearboxes",
+	weight = 150,
+	maxtq = MaxTqMedium
+} )
+
+ACF_DefineGearboxAir( "air_gearbox2-b", {
+	name = "Airplane Gearbox Short Large",
+	desc = "A  short large airplane gearbox. Used to Power a propeller.\nYou SHOULD use a gearbox to power this gearbox",
+	model = "models/engines/flywheelclutchb.mdl",
 	category = "Airplane Gearboxes",
 	weight = 200,
 	maxtq = MaxTqLarge
